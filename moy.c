c3c792dfea6dd58f6cd1d957276c6a49d88f948d
Evariste Bigmac <102599657+Evariste-Bigmac@users.noreply.github.com>
2022-05-03 15:42:16 +0200

    Ajout des sous fonctions moyenne et écart type

---
+++ moy.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "moy.h"
+
+// LONGUEUR : NOMBRE D'ECHANTILLONS PAR PISTE
+// LARGEUR : NOMBRE DE PISTES
+
+double moyenne(double* data, int longueur) {
+	double M = 0;
+	for(int i = 0; i < longueur; i++) {
+		M += data[i];
+	}
+	M = M / longueur;
+	return(M);
+}
+
+double ecart(double* data, int longueur) {
+	double S = 0;
+	double M = moyenne(data, longueur);
+	for(int i = 0; i < longueur; i ++) {
+		S += (data[i] - M) * (data[i] - M);
+	}
+	S = S / longueur;
+	S = sqrt(S);
+	return(S);
+}
+
+double* mvect (double* matrice, int longueur_ech, int nb_ech) {
+	double* res = malloc(nb_ech * sizeof(double));
+	for(int k = 0; k < nb_ech; k ++) {
+		double intermedivecteur[longueur_ech];
+		for(int i = 0; i < longueur_ech; i ++) {
+			intermedivecteur[i] = matrice[i + k*longueur_ech];
+		}
+		double M = moyenne(intermedivecteur, longueur_ech);
+		res[k] = M;
+}
+	return (res);
+}
+
+double* ecvect (double* matrice, int longueur_ech, int nb_ech) {
+	double* resu = malloc(nb_ech * sizeof(double));
+	for(int k = 0; k < nb_ech; k ++) {
+		double intermedivecteur[longueur_ech];
+		for(int i = 0; i < longueur_ech; i ++) {
+			intermedivecteur[i] = matrice[i + k*longueur_ech];
+		}
+		double M = ecart(intermedivecteur, longueur_ech);
+		resu[k] = M;
+}
+	return (resu);
+}
+
+/*
+int main() {
+	double matrice [20];
+	for(int i = 0; i < 20; i++) {
+		matrice[i] = i;
+	}
+	double *moyvect =  mvect(matrice, 4, 5);
+	printf("%f\n %f\n %f\n %f\n %f\n", moyvect[0], moyvect[1], moyvect[2], moyvect[3], moyvect[4]);
+	free(moyvect);
+
+	double *vectecart = ecvect(matrice, 4, 5);
+	printf("%f\n %f\n %f\n %f\n %f\n", vectecart[0], vectecart[1], vectecart[2], vectecart[3], vectecart[4]);
+	free(vectecart);
+}*/
---
+++ moy.h
@@ -0,0 +1,10 @@
+#ifndef __MOY_H__
+#define __MOY_H__
+
+double moyenne(double* data, int longueur);
+double ecart(double* data, int longueur);
+double* mvect (double* matrice, int longueur_ech, int nb_ech);
+double* ecvect (double* matrice, int longueur_ech, int nb_ech);
+
+
+#endif
